d47e96002b7a81aa1a9493fba08e8653dc116072
teodorp11 <teodor.posea@gmail.com>
2025-12-14 12:47:51 +0200

    Add the search functionality.

--- Core/Specifications/ProductSpecificationParameters.cs
+++ Core/Specifications/ProductSpecificationParameters.cs
@@ -33,4 +33,12 @@ public class ProductSpecificationParameters
     }
 
     public string? Sort { get; set; }
+
+    private string? _search;
+    public string Search
+    {
+        get => _search ?? "";
+        set => _search = value.ToLower();
+    }
+    
 }
