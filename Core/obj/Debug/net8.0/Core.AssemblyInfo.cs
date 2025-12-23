3b1553752300844e27d61b783b48ff4e227a171b
teodorp11 <teodor.posea@gmail.com>
2025-12-23 14:21:29 +0200

    Implement an HTTP Interceptor for handling API errors.

--- Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
+++ Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+547adf553f92167ca2eb256b7301bc3f423cfa5a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1316e66a4ea37f3e8890e7924b7a7c0aa10a1114")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
