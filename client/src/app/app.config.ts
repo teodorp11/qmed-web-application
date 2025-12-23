24e413e8b72b0df763d313c8f3120f6303e9e5bb
teodorp11 <teodor.posea@gmail.com>
2025-12-23 15:13:12 +0200

    Implement an HTTP Interceptor for loading.

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -8,12 +8,13 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { errorInterceptor } from './core/interceptors/error-interceptor';
+import { loadingInterceptor } from './core/interceptors/loading-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(withInterceptors([errorInterceptor])),
+    provideHttpClient(withInterceptors([errorInterceptor, loadingInterceptor])),
   ],
 };
