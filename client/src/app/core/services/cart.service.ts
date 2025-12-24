2eb7891c575f2ce1a98d6a8aa4733373f624a009
teodorp11 <teodor.posea@gmail.com>
2025-12-24 16:36:19 +0200

    Implement the getCart() and setCart() methods.

--- client/src/app/core/services/cart.service.ts
+++ client/src/app/core/services/cart.service.ts
@@ -1,8 +1,31 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Cart } from '../../shared/models/cart';
+import { map, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CartService {
-  
+  baseUrl = environment.apiUrl;
+  private http = inject(HttpClient);
+  cart = signal<Cart | null>(null);
+
+  getCart(id: string) {
+    return this.http.get<Cart>(this.baseUrl + 'cart?id=' + id).pipe(
+      map((cart) => {
+        this.cart.set(cart);
+        return cart;
+      })
+    );
+  }
+
+  setCart(cart: Cart) {
+    return this.http.post<Cart>(this.baseUrl + 'cart', cart).pipe(
+      tap((cart) => {
+        this.cart.set(cart);
+      })
+    );
+  }
 }
