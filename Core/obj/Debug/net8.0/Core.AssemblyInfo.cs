9c91c46833d66942ffd9b23554e057b92ac3d3ab
teodorp11 <teodor.posea@gmail.com>
2025-12-24 16:23:12 +0200

    Create the Cart components.

--- Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
+++ Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08bcd78b0d451597a5eddd242bcfb2aafe024c2e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff4ebd0fe74bfbe2b75651a8811ac0652176091c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
