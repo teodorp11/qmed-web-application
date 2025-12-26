9563db54d03ac9ea63111c7b5ae1189ab59d7c1e
teodorp11 <teodor.posea@gmail.com>
2025-12-26 14:49:29 +0200

    refactor: Calculate total order summary sum.

--- client/src/app/core/services/cart.service.ts
+++ client/src/app/core/services/cart.service.ts
@@ -15,6 +15,22 @@ export class CartService {
   itemCount = computed(() => {
     return this.cart()?.items.reduce((sum, item) => sum + item.quantity, 0);
   });
+  totals = computed(() => {
+    const cart = this.cart();
+
+    if (!cart) return null;
+    const subtotal = cart.items.reduce((sum, item) => sum + item.price * item.quantity, 0);
+
+    const delivery = 0;
+    const discountValue = 0;
+
+    return {
+      subtotal,
+      delivery,
+      discount: discountValue,
+      total: subtotal + delivery - discountValue,
+    };
+  });
 
   getCart(id: string) {
     return this.http.get<Cart>(this.baseUrl + 'cart?id=' + id).pipe(
--- client/src/app/shared/components/order-summary/order-summary.component.ts
+++ client/src/app/shared/components/order-summary/order-summary.component.ts
@@ -1,15 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatButton } from '@angular/material/button';
 import { MatFormField, MatLabel } from '@angular/material/form-field';
 import { MatInput } from '@angular/material/input';
 import { RouterLink } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { CartService } from '../../../core/services/cart.service';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-order-summary',
   standalone: true,
-  imports: [MatButton, RouterLink, MatFormField, MatLabel, MatInput, FormsModule],
+  imports: [MatButton, RouterLink, MatFormField, MatLabel, MatInput, FormsModule, CurrencyPipe],
   templateUrl: './order-summary.component.html',
   styleUrl: './order-summary.component.scss',
 })
-export class OrderSummaryComponent {}
+export class OrderSummaryComponent {
+  cartService = inject(CartService);
+}
