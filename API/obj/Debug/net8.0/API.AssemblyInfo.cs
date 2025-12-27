741a244148a0a5755ea6a60ec279bc97e03ed98f
teodorp11 <teodor.posea@gmail.com>
2025-12-27 18:37:23 +0200

    feat: create Login and Register components and implement the Account service component

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e89000768023327d02148ae06a0179df6b6cb7e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1e264f533e443213e6b7c70c431dcb305f0d39b")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
