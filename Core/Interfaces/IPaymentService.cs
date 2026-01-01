37786553d252597572986c556b7a7c22df438fb8
teodorp11 <teodor.posea@gmail.com>
2026-01-01 22:29:34 +0200

    feat: implement the CreateOrUpdatePaymentIntent() method

--- Core/Interfaces/IPaymentService.cs
+++ Core/Interfaces/IPaymentService.cs
@@ -5,5 +5,5 @@ namespace Core.Interfaces;
 
 public interface IPaymentService
 {
-    Task<ShoppingCart> CreateOrUpdatePaymentIntent(string cartId);
+    Task<ShoppingCart?> CreateOrUpdatePaymentIntent(string cartId);
 }
