3065c566e3e674e0e3519c517009796ba886ce4e
teodorp11 <teodor.posea@gmail.com>
2026-01-04 00:09:38 +0200

    feat: add client side Stripe client

--- client/src/environments/environment.development.ts
+++ client/src/environments/environment.development.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: false,
   apiUrl: 'https://localhost:5001/api/',
+  stripePublicKey:
+    'pk_test_51SjQMpBpMCaLtamxHxOWjJnlMsYwVedfNK1RMrO7y3pKuXMG4v2dmXbYwOHOp2nyemT02k9XaOWqjquLtm0LJ0wl003Vr2Tvkd',
 };
--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
   apiUrl: 'api/',
+  stripePublicKey:
+    'pk_test_51SjQMpBpMCaLtamxHxOWjJnlMsYwVedfNK1RMrO7y3pKuXMG4v2dmXbYwOHOp2nyemT02k9XaOWqjquLtm0LJ0wl003Vr2Tvkd',
 };
