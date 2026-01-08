8318c534c5e482a43d9b7ec0295a738e6463729c
teodorp11 <teodor.posea@gmail.com>
2026-01-08 22:10:36 +0200

    feat(checkout): integrate Stripe Payment and Address elements

--- client/src/app/core/services/stripe.service.ts
+++ client/src/app/core/services/stripe.service.ts
@@ -5,6 +5,7 @@ import {
   StripeAddressElement,
   StripeAddressElementOptions,
   StripeElements,
+  StripePaymentElement,
 } from '@stripe/stripe-js';
 import { environment } from '../../../environments/environment';
 import { CartService } from './cart.service';
@@ -24,6 +25,7 @@ export class StripeService {
   private stripePromise: Promise<Stripe | null>;
   private elements?: StripeElements;
   private addressElement?: StripeAddressElement;
+  private paymentElement?: StripePaymentElement;
 
   constructor() {
     this.stripePromise = loadStripe(environment.stripePublicKey);
@@ -47,6 +49,18 @@ export class StripeService {
       }
     }
     return this.elements;
+  }
+
+  async createPaymentElement() {
+    if (!this.paymentElement) {
+      const elements = await this.initializeElements();
+      if (elements) {
+        this.paymentElement = elements.create('payment');
+      } else {
+        throw new Error('Elements instance has not been initialized.');
+      }
+    }
+    return this.paymentElement;
   }
 
   async createAddressElement() {
@@ -100,5 +114,6 @@ export class StripeService {
   disposeElements() {
     this.elements = undefined;
     this.addressElement = undefined;
+    this.paymentElement = undefined;
   }
 }
