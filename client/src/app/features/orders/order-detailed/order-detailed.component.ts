d163bac83c6c2ea5bc372247526028cc5d95cdc5
teodorp11 <teodor.posea@gmail.com>
2026-01-17 16:44:12 +0200

    feat(orders): implement detailed order summary view
    - Implement logic in OrderDetailedComponent to fetch order data by ID using ActivatedRoute
    - Add UI template for order details including status, date, and buyer information
    - Create a detailed order items table with product images, quantities, and pricing
    - Implement an order summary section displaying subtotal, discount, shipping, and total
    - Integrate MatCardModule for the layout and Currency/Date pipes for data formatting

--- client/src/app/features/orders/order-detailed/order-detailed.component.ts
+++ client/src/app/features/orders/order-detailed/order-detailed.component.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { OrderService } from '../../../core/services/order.service';
+import { ActivatedRoute } from '@angular/router';
+import { Order } from '../../../shared/models/order';
+import { MatCardModule } from '@angular/material/card';
+import { MatButton } from '@angular/material/button';
+import { CurrencyPipe, DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-order-detailed',
-  imports: [],
+  standalone: true,
+  imports: [MatCardModule, MatButton, CurrencyPipe, DatePipe],
   templateUrl: './order-detailed.component.html',
   styleUrl: './order-detailed.component.scss',
 })
-export class OrderDetailedComponent {
+export class OrderDetailedComponent implements OnInit {
+  private orderService = inject(OrderService);
+  private activatedRoute = inject(ActivatedRoute);
+  order?: Order;
 
+  ngOnInit(): void {
+    this.loadOrder();
+  }
+
+  loadOrder() {
+    const id = this.activatedRoute.snapshot.paramMap.get('id');
+    if (!id) return;
+
+    this.orderService.getOrderDetailed(+id).subscribe({
+      next: (order) => (this.order = order),
+    });
+  }
 }
